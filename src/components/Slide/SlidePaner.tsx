e966c45e509aa9aec66961921796f8b6dd6fc001
Nguyễn Phước Công <88037699+NPC2704@users.noreply.github.com>
2023-07-13 17:08:44 +0700

    up

---
+++ src/components/Slide/SlidePaner.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/logo.jpg";
+import axios from "axios";
+const SlidePaner = () => {
+  const [data1, setData] = useState<any[]>([]);
+  const [datatitle, setDatatitle] = useState("");
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://apisolfive.app.tranviet.site/api/get/home"
+      );
+      setData(response.data?.data?.data?.items?.[2] || []);
+      console.log(response.data?.data?.data?.items?.[2]?.items);
+      setDatatitle(response.data?.data?.data?.items?.[2]?.title);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <div className="w-full h-100">
+      <div className="w-full h-20 flex items-center justify-start">
+        <h1 className="text-white text-4xl font-bold">{datatitle}</h1>
+      </div>
+      <div className="w-full h-90 ">
+        <div className="flex justify-around w-full h-80 ">
+          <div className="h-12 w-60  mr-4 mt-2 flex items-center border-4  border-${#fff} border-solid rounded">
+            <div className="ml-4 ">
+              <div className="flex justify-start">
+                <p className="text-white text-lg">All</p>
+              </div>
+            </div>
+          </div>
+          <div className="h-12 w-60  mr-4 mt-2 flex items-center border-4  border-${#fff} border-solid rounded">
+            <div className="ml-4 ">
+              <div className="flex justify-start">
+                <p className="text-white text-lg">Others</p>
+              </div>
+            </div>
+          </div>
+          <div className="h-12 w-60  mr-4 mt-2 flex items-center border-4  border-${#fff} border-solid rounded">
+            <div className="ml-4 ">
+              <div className="flex justify-start">
+                <p className="text-white text-lg">vPops</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SlidePaner;
--- src/components/Slide/Slides.tsx
+++ src/components/Slide/Slides.tsx
@@ -1,11 +1,13 @@
 import HeaderSlide from "./HeaderSlide";
 import BodySlide from "./BodySlide";
+import SlidePaner from "./SlidePaner";
 import { AppProvider2 } from "../../Context/AppContext";
 const Slides = () => {
   return (
     <div className=" w-full h-128 bg-black">
       <div className="max-w-7xl h-full mx-auto">
         <HeaderSlide />
+        <SlidePaner />
         <AppProvider2>
           <BodySlide />
         </AppProvider2>
